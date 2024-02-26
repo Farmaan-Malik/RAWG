86e76e37a25af05eb02b02fa53e125ffdfb6e915
Farmaan-Malik <farmaanmalik285@gmail.com>
2024-02-26 16:19:40 +0530

    create new GameCard

--- app/src/main/java/com/example/rawg/presentation/GamesListScreen/components/GameCard.kt
+++ app/src/main/java/com/example/rawg/presentation/GamesListScreen/components/GameCard.kt
@@ -6,13 +6,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -22,12 +25,15 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -45,64 +51,52 @@ fun GameCard(
 //    navController: NavHostController,
    navigate: ()-> Unit
 ) {
-//    val color= "0xFF${dominant_color}"
-    Log.e("LMAO2", id.toString() )
-    Card(
-        modifier = Modifier
-            .padding(horizontal = 8.dp, vertical = 16.dp)
-            .background(
-                color = Color(0xFF00BE67),
-                shape = RoundedCornerShape(corner = CornerSize(12.dp))
-            )
-            .fillMaxWidth()
-            .fillMaxSize()
-            .clickable { navigate()
-//                navController.navigate("GameDetailScreen/{$id}")
-                }
-            .shadow(
-                elevation = 10.dp,
-                ambientColor = Color.Red
-
-            )
+    Card(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)
+        .wrapContentSize(Alignment.BottomCenter)
     ) {
-        Row(
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 16.dp)
+            .height(200.dp)
+            .fillMaxHeight(),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Image(
             modifier = Modifier
-                .height(250.dp)
                 .fillMaxSize()
-                .background(
-                    brush = Brush.horizontalGradient(
-                        colors = listOf(
-                            Color(0xFF00A1D5),
-                            Color(0xFFF500BD),
-                        ),
-                    )
-                ))
-        {
-            Image(
-                painter = rememberAsyncImagePainter(background_image),
-                contentDescription = null,
-                modifier = Modifier
-                    .padding(15.dp)
-                    .fillMaxWidth(.5f)
-                    .fillMaxHeight(1f),
-                contentScale = ContentScale.FillHeight
-                //.aspectRatio(1f)
-            )
-            Column (modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                ){
+//                .border(width = 5.dp, color = Color.Blue)
+                .aspectRatio(1.5f),
+            painter = rememberAsyncImagePainter(background_image),
+            contentDescription = "${name} Image",
+            contentScale = ContentScale.Crop
+        )
+        Column {
+            Text(text = name, fontWeight = FontWeight.Bold, fontSize = 25.sp)
+            Text(text = released)
+            Row() {
+                for (x in 1..rating.toInt()) {
+                    Icon(
+                        imageVector = Icons.Default.Star,
+                        contentDescription = "",
+                        Modifier
+                            .shadow(elevation = 100.dp)
+//                            .padding(top = 50.dp)
+                            .background(
+                                brush = Brush.radialGradient(
+                                    colors = listOf(
+                                        Color.Black,
+                                        Color.Transparent
+                                    )
+                                )
+                            )
 
-                Text(name, fontSize = 25.sp, modifier = Modifier.padding(bottom = 10.dp))
-                Text(text = released)
-                Row {
-                    for(x in 1.. rating.toInt()){
-                        Icon(imageVector = Icons.Default.Star, contentDescription = "")
-                    }
+                    )
                 }
             }
-
         }
-
-
     }
+}
 }
