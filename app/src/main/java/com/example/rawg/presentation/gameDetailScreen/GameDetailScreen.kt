a799029f2cebd7246a6d3d7ab7064d314abef43d
Farmaan-Malik <farmaanmalik285@gmail.com>
2024-03-01 12:46:55 +0530

    Update UI

--- app/src/main/java/com/example/rawg/presentation/gameDetailScreen/GameDetailScreen.kt
+++ app/src/main/java/com/example/rawg/presentation/gameDetailScreen/GameDetailScreen.kt
@@ -14,7 +14,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -31,19 +33,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import coil.compose.rememberAsyncImagePainter
 import com.example.rawg.presentation.common.LoadingScreen
+import com.example.rawg.presentation.common.MyAppBar
 import org.koin.androidx.compose.koinViewModel
 
 
-@OptIn( ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GameDetailScreen
-            (viewModel: GameDetailScreenViewModel = koinViewModel(),
-             id:Int, navController: NavHostController) {
+fun GameDetailScreen(
+    viewModel: GameDetailScreenViewModel = koinViewModel(),
+    id: Int, navController: NavHostController
+) {
 
     val isLoading by remember { viewModel.isLoading }
     val scaffoldState = rememberBottomSheetScaffoldState()
@@ -63,30 +69,48 @@ fun GameDetailScreen
 
         BottomSheetScaffold(
             scaffoldState = scaffoldState,
-            sheetPeekHeight = 380.dp,
-            sheetContent = { Box(modifier = Modifier.fillMaxSize()){
-                Text(viewModel._gameDetails.description_raw, fontSize = 20.sp)
-            }
-                 })
-        {
+            topBar = { MyAppBar() },
+            sheetPeekHeight = 330.dp,
+            sheetContainerColor = Color(0xFFC499F3),
+            sheetShape = AbsoluteRoundedCornerShape(18),
+            sheetContent = {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Column {
 
-//        Box(modifier = Modifier.fillMaxSize())
-                PaddinValues->
+                        Text(text = "Description: ",
+                            fontSize = 20.sp,
+                            color = Color(0xFF160735),
+                            modifier = Modifier
+                                .padding(horizontal = 20.dp)
+                                .verticalScroll(ScrollState(0)))
+                        Text(
+                            viewModel._gameDetails.description_raw,
+                            fontSize = 20.sp,
+                            color = Color(0xFF160735),
+                            modifier = Modifier
+                                .padding(20.dp)
+                                .verticalScroll(ScrollState(0))
+                        )
+                    }
+                }
+            })
+        { PaddinValues ->
             Column(
                 modifier = Modifier
-                    .fillMaxSize()
-//                    .padding(20.dp)
-                ,
-                horizontalAlignment = Alignment.CenterHorizontally
+                    .fillMaxSize().background(brush = Brush.verticalGradient(listOf(
+                    Color(0xFF7360DF),
+                    Color(0xFF160735),
+                    Color(0xFFC499F3)
+                ))),
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 Image(
                     painter = rememberAsyncImagePainter(viewModel._gameDetails.background_image),
                     contentDescription = "${viewModel._gameDetails.name} Image",
                     modifier = Modifier
                         .fillMaxWidth()
-//                    .padding(top = 8.dp)
-                        .fillMaxHeight(.35f)
-                        .border(5.dp, Color.Black),
+                        .padding(16.dp)
+                        .fillMaxHeight(.35f),
                     contentScale = ContentScale.FillBounds
                 )
                 Text(
@@ -94,7 +118,8 @@ fun GameDetailScreen
                     modifier = Modifier
                         .padding(top = 8.dp),
                     fontSize = 25.sp,
-                    color = Color.DarkGray
+                    fontWeight = FontWeight.ExtraBold,
+                    color = Color(0xFFE5CFF7)
                 )
                 Row(
                     modifier = Modifier
@@ -104,45 +129,63 @@ fun GameDetailScreen
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
 
-                    Row (verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center){
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
                         Text(
                             "Rating: ", modifier = Modifier,
                             fontSize = 20.sp,
-                            color = Color.DarkGray
+                            fontWeight = FontWeight.SemiBold,
+                            color = Color(0xFFE5CFF7)
                         )
-                            Text(
-                                "${viewModel._gameDetails.rating}",
-                                modifier = Modifier,
-                                fontSize = 18.sp,
-                                color = Color.DarkGray
-                            )
+                        Text(
+                            "${viewModel._gameDetails.rating}",
+                            modifier = Modifier,
+                            fontSize = 18.sp,
+                            color = Color(0xFFE5CFF7),
+                            fontWeight = FontWeight.Medium
+                        )
                     }
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center) {
+                        horizontalArrangement = Arrangement.Center
+                    ) {
                         for (x in 1..viewModel._gameDetails.rating.toInt()) {
-                            Icon(imageVector = Icons.Default.Star, contentDescription = null, tint = Color(0xFFFFD700))
+                            Icon(
+                                imageVector = Icons.Default.Star,
+                                contentDescription = null,
+                                tint = Color(0xFFFFD700)
+                            )
                         }
                     }
                 }
-                
-                Box (
-                    modifier = Modifier.padding(horizontal = 10.dp).background(Color.Black, shape = RoundedCornerShape(80))
-                ){
-                    Row (modifier = Modifier
-                        .fillMaxWidth()
-                        .horizontalScroll(state = ScrollState(0)),
-                        horizontalArrangement = Arrangement.SpaceAround,){
+
+                Box(
+                    modifier = Modifier
+                        .padding(horizontal = 10.dp)
+                        .background(
+                            Color(0xFFF2AFEF),
+                            shape = RoundedCornerShape(80)
+                        )
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .horizontalScroll(state = ScrollState(0)),
+                        horizontalArrangement = Arrangement.SpaceAround,
+                    ) {
                         viewModel._gameDetails.tags.forEach { tag ->
-                            Text(tag.name, modifier = Modifier
-                                .padding(8.dp)
-                                .padding(5.dp),
-                                color = Color.White)
+                            Text(
+                                tag.name, modifier = Modifier
+                                    .padding(8.dp)
+                                    .padding(5.dp),
+                                color = Color.Black
+                            )
                         }
                     }
                 }
             }
         }
+    }
 }
-            }
